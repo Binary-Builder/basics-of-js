33e574185120d59dc20853ef644b2f74a37e9342
Shreeya Bansal <shreeyabansal77@gmail.com>
2025-08-16 19:49:40 +0530

    bogos binted

---
+++ section 2/variables/index.js
@@ -0,0 +1,7 @@
+let anc='bogos binted?';
+console.log(anc);
+
+//cannot be reserved keyword (duhh)
+//should be meaningful (duhhhx2)
+//cannot start with a number
+//cannot contain a space or hyphen (-)
